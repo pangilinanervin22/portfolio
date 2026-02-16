db97052765c208ff18fc2f983fe873eee6ca035a
Ervin Pangilinan <pangilinanervin22@gmail.com>
2026-02-17 07:13:00 +0800

    1.5.1 feat: add development environment variables and implement dynamic robots.txt response

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,15 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from '@astrojs/react';
 
-// https://astro.build/config
-const env = loadEnv(process.env.NODE_ENV ?? "production", process.cwd(), "");
+const env = loadEnv(process.env.NODE_ENV ?? "development", process.cwd(), "");
 
 export default defineConfig({
-  // Use env to override site URL and base path per deployment target
   site: env.DEFAULT_PATH || "https://pangilinanervin22.github.io/",
   base: env.DEFAULT_BASE || "/portfolio",
   integrations: [mdx(), sitemap(), react()],
   output: "static",
   experimental: {
-    svgo : true
+    svgo: true
   }
 });
 
